199e0a2c5fbba880c2f561822235dbc43e0642fb
Xavierito1825 <xavier.bermejo.sotillo@estudiantat.upc.edu>
2023-06-05 21:21:47 +0200

    Se introducen servicios correctamente
    Solo falta añadir trabajadores con un rol por cada servicio

--- proyecte_django/brigada/serveis_info.py
+++ proyecte_django/brigada/serveis_info.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from .models import Rol, Servei, Especial, Quotidia
+from .models import DiaTreball, Rol, Servei, Especial, Quotidia
+
+from django import forms
+
+
+class ServeiForm(forms.Form):
+    dia = forms.DateField(label='Día', widget=forms.DateInput(attrs={'class': 'form-control', 'required': True}))
+    Carrer = forms.CharField(label='Carrer', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Av. ...'}))
+    Numero = forms.IntegerField(label='Numero', widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '123 ...'}))
+    Alt_Prof = forms.IntegerField(label='Altura / Profunditat', widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '[0-7]'}))
+    tipus_Servei = forms.ChoiceField(label='Tipus de servei', choices=[('Especial', 'Especial'), ('Quotidia', 'Quotidia')], widget=forms.Select(attrs={'class': 'form-control', 'required': True}))
 
 def lista_serveis(request):
    if request.method == 'POST':
@@ -16,5 +26,22 @@ def lista_serveis(request):
 
                context = {'dia_servei': dia, 'serveis_dia': servei_dia, 'roles': roles, 'especial': especial, 'quotidia': quotidia}
                return render(request, 'Serveis/Serveis_list.html', context)
-
+         
+      # Obtener la lista de trabajadores para mostrar en el formulario
+      else:        
+        carrer = request.POST.get('Carrer')
+        numero = request.POST.get('Numero')
+        dia = request.POST.get('dia')
+        tipus_servei = request.POST.get('tipus_Servei')
+        dia_treball, created = DiaTreball.objects.get_or_create(Dia=dia) 
+        if tipus_servei == 'Especial':
+            profunditat = request.POST.get('Alt/Prof')
+            
+            servei = Especial(Carrer=carrer, Numero=numero, Dia=dia_treball, profunditat=profunditat)
+            servei.save()
+        else:
+            altura = request.POST.get('Alt/Prof')
+            servei = Quotidia(Carrer=carrer, Numero=numero, Dia=dia_treball, altura=altura)
+            servei.save()
+            
    return render(request, 'Serveis/Serveis_list.html')
